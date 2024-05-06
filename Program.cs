a45681898fe1c22fbd76db2062e1f48ac5e11c3a
Julio Bacoli <julio-microlins@hotmail.com>
2024-05-05 22:58:01 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("====================================");
+Console.WriteLine("Bem vindo ao projeto CALCULADORA!");
+Console.WriteLine("====================================");
+
+Console.WriteLine("Digite o PRIMEIRO valor: ");
+float valor1 = float.Parse(Console.ReadLine());
